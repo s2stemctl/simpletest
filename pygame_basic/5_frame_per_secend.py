f384bd49584456e88805b659310ee1de34489b31
mongmin <show5323@nate.com>
2021-10-26 22:35:15 +0900

    [update] enemy 생성, 충돌시 프로그램 종료 개발완료

--- pygame_basic/5_frame_per_secend.py
+++ pygame_basic/5_frame_per_secend.py
@@ -29,6 +29,14 @@ to_y=0
 
 #이동속도
 character_speed=0.5
+
+# 적 enemy
+enemy=pygame.image.load("C:\\Users\\Mongmin\\Documents\\simpletest\\pygame_basic\\background\\enemy.jpg")
+enemy_size=enemy.get_rect().size # 이미지의 크기를 가져옴
+enemy_width=enemy_size[0] # 케릭터 가로크기
+enemy_hight=enemy_size[1] # 케릭터 세로크기
+enemy_x_pos=(screen_width/2) - (enemy_width/2) # 화면 가로의 절반 크기에 해당하는곳에 위치
+enemy_y_pos=(screen_height/2)-(enemy_hight/2) # 화면 세로의 절반 
 # 이벤트 루프
 running=True #  게임 진행 중인지 확인
 while running:
@@ -66,8 +74,25 @@ while running:
     elif character_y_pos>screen_height-character_hight:
         character_y_pos=screen_height-character_hight
 
+    # 충돌처리
+    character_rect=character.get_rect()
+    character_rect.left=character_x_pos
+    character_rect.top=character_y_pos
+
+    enemy_rect=enemy.get_rect()
+    enemy_rect.left=enemy_x_pos
+    enemy_rect.top=enemy_y_pos
+    
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌했어요")
+        running=False
+
+
+
     screen.blit(background,(0,0)) # 배경그리기
     screen.blit(character,(character_x_pos,character_y_pos))
+    screen.blit(enemy,(enemy_x_pos,enemy_y_pos))
     pygame.display.update() # 게임화면 다시그리기
 # pygame 종료
 pygame.quit()
